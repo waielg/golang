e3dcc95178b36c472153b1d52298816d8aee0272
waielg <145469925+waielg@users.noreply.github.com>
2023-09-20 01:12:53 -0400

    Add files via upload

---
+++ day2.go
@@ -0,0 +1,18 @@
+// replicate day 1
+
+package main
+
+import "fmt"
+
+func main(
+	fmt.Println("Lets ,see if this works") 
+)
+
+// add 2 parameters
+
+func adder(parameter1 int, parameter2 int) int {
+	return parameter1 + parameter2
+}
+
+adder(1,4)
+
